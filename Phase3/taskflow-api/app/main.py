9c3ca89bf5ad2ed2babf43fc1b2fb9c41dc5ba9f
dami <dami@C-PF1AU29J.localdomain>
2026-05-04 10:12:28 +0100

    Day 19 cloud run deployment

--- Phase3/taskflow-api/app/main.py
+++ Phase3/taskflow-api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes import router
+import os
 
 
 def create_app():
@@ -7,5 +8,5 @@ def create_app():
     app.include_router(router)
     return app
 
-
+port = int(os.environ.get("PORT", 8000))
 app = create_app()
