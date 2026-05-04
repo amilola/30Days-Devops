b8303293a9f91b59bdcc1d67c26d72e43678977c
dami <dami@C-PF1AU29J.localdomain>
2026-05-04 10:50:43 +0100

    Fix Cloud Run port issue

--- Phase3/taskflow-api/app/main.py
+++ Phase3/taskflow-api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes import router
 import os
+import uvicorn
 
 
 def create_app():
@@ -9,5 +10,11 @@ def create_app():
     return app
 
 
-port = int(os.environ.get("PORT", 8000))
 app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+
+
